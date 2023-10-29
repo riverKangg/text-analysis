c0a2a7819d119960388f4690377c6d396c93bbd7
riverKangg <riverKangg@gmail.com>
2023-10-30 06:39:44 +0900

    Reordered Tokenizer Invocation Sequence and Renamed Variables for Clarity.

--- utils/load_preprocessed_translation_data.py
+++ utils/load_preprocessed_translation_data.py
@@ -1,11 +1,14 @@
 import datasets
+from transformers import MBart50TokenizerFast
 from datasets.features.translation import Translation
 
-huggingface_path = "riverkangg/aihub-ko-en-translation-socsci"
-data = datasets.load_dataset(huggingface_path)
+path = "riverkangg/aihub-ko-en-translation-socsci"
+
+model_name = "facebook/mbart-large-50-many-to-many-mmt"
+tokenizer = MBart50TokenizerFast.from_pretrained(model_name, src_lang="en_XX", tgt_lang="ko_KR")
 
 
-def preprocess_function(examples):
+def tokenize_translation_data(examples):
     inputs = [ex["en"] for ex in examples["translation"]]
     targets = [ex["ko"] for ex in examples["translation"]]
     tokens = tokenizer(inputs, text_target=targets, max_length=128,
@@ -13,22 +16,16 @@ def preprocess_function(examples):
     return tokens
 
 
-def LoadPreprocessedTranslationData(tokenizer, huggingface_path="riverkangg/aihub-ko-en-translation-socsci"):
-    data = datasets.load_dataset(huggingface_path)
+def load_translation_data(path=path):
+    data = datasets.load_dataset(path)
     data = data.cast_column('translation', Translation(languages=['en', 'ko']))
 
-    model_input = data.map(preprocess_function, batched=True)
+    model_input = data.map(tokenize_translation_data, batched=True)
 
     return model_input
 
 
 if __name__ == '__main__':
-    from transformers import MBart50TokenizerFast
-
-    model_name = "facebook/mbart-large-50-many-to-many-mmt"
-    tokenizer = MBart50TokenizerFast.from_pretrained(model_name, src_lang="en_XX", tgt_lang="ko_KR")
-
     path = "riverkangg/aihub-ko-en-translation-socsci"
-    tokenized_datasets = LoadPreprocessedTranslationData(tokenizer, huggingface_path=path)
-
+    tokenized_datasets = load_translation_data(path=path)
     print(tokenized_datasets)
